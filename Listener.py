e02fba8d1de523112ddbc615a9876a76b7b66877
leewinn1974 <107724581+leewinn1974@users.noreply.github.com>
2022-07-18 17:34:35 -0800

    Add files via upload

--- Listener.py
+++ Listener.py
@@ -5,8 +5,8 @@ def run_mavlink_reciever():
     print('Mavlink Reciever Started')
     app = App.get_running_app()
 
-    # Start a connection listening on a UDP port
-    the_connection = mavutil.mavlink_connection('tcp:localhost:14551')
+    # Start a connection listening on a TCP port
+    the_connection = mavutil.mavlink_connection('tcp:localhost:14550') # Changed to mission Planner default port. 
 
     # Wait for the first heartbeat 
     #   This sets the system and component ID of remote system for the link
@@ -14,17 +14,25 @@ def run_mavlink_reciever():
     print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
     # Once connected, use 'the_connection' to get and send messages
-    time = 0
-    while 1:
-        msg = the_connection.recv_match(blocking=True)
+    
+    while True:
+
+        #This seems cleaner than IF statements
+        time = the_connection.recv_match(type='GPS_RAW_INT', blocking=True).time_usec
+        velocity = the_connection.recv_match(type='GPS_RAW_INT', blocking=True).vel
+        
+        print_txt = 'GPS Time: ' + str(time)    
+        print_txt = 'Vel: ' + str(velocity)
+
+
         #print(msg)
-        if msg.name == 'GPS_RAW_INT':
-            time = msg.time_usec
-            velocity=msg.vel
-            print_txt = 'GPS Time: '+ str(time)
-            print_txt_vel = 'GPS Vel: '+ str(velocity)
-            app.root.gps_text.text = print_txt
-            app.root.vel_text.text = print_txt_vel
+        # if msg.name == 'GPS_RAW_INT':
+        #     time = msg.time_usec
+        #     velocity=msg.vel
+        #     print_txt = 'GPS Time: '+ str(time)
+        #     print_txt_vel = 'GPS Vel: '+ str(velocity)
+        # app.root.gps_text.text = print_txt
+        # app.root.vel_text.text = print_txt_vel
             #print(print_txt)
         
 
