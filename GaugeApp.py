06d45ecfa4a1d21338552759d25affb9e50b1cfe
leewinn1974 <107724581+leewinn1974@users.noreply.github.com>
2022-08-21 13:39:58 -0800

    Battery Warning Logic Update

--- GaugeApp.py
+++ GaugeApp.py
@@ -27,7 +27,7 @@ class GaugeApp(App):
         
         def try_again():               
             ids.link.text = 'Retry?'
-            self.warn_mgr(link_warn, 'red', 0, 0)
+            self.warn_mgr(link_warn, 'red', 0, '', 0)
 
         if platform == 'linux' or platform == 'linux2':
             connection_str = 'udpin:localhost:14550' # Might only work with SITL
@@ -39,7 +39,7 @@ class GaugeApp(App):
             connected = self.connection.wait_heartbeat(timeout=2)
             if connected != None:
                 ids.link.text = 'Connected'                
-                self.warn_mgr(link_warn, 'green', 0, 0)                
+                self.warn_mgr(link_warn, 'green', 0, '', 0)                
                 Clock.schedule_once(self.scheduler, 0.1) 
             else:
                 try_again()          
@@ -48,25 +48,25 @@ class GaugeApp(App):
             try_again()
     
     # This manages the color of the warning "lights" and shadows        
-    def warn_mgr(self, id, color, shadow, battery):
+    def warn_mgr(self, id, color, sec, sh_col, battery):
         ids = self.root.ids
 
         id.color = self.rgba_colors['black']  # Sets the button text color to black        
         id.background_color = self.rgba_colors[color]
 
-        if shadow == 1:
+        if sec == 1:
             if id == ids.fuel_warn:
                 new_id = ids.level
-                new_id.shadow_color = self.hex_colors[color]
+                new_id.shadow_color = self.hex_colors[sh_col]
 
             if id == ids.batt_warn:
                 if battery == 1:
                     batt_id = ids.batt1
-                    batt_id.shadow_color = self.hex_colors[color]
+                    batt_id.shadow_color = self.hex_colors[sh_col]
                     
                 if battery == 2:
                     batt_id = ids.batt2
-                    batt_id.shadow_color = self.hex_colors[color]
+                    batt_id.shadow_color = self.hex_colors[sh_col]
                     
     # This sets values and defines ranges for the primary gauges
     def primary_update(self, dt):
@@ -92,16 +92,16 @@ class GaugeApp(App):
         temp_warn = ids.temp_warn
         
         if cht_val < 80:
-            self.warn_mgr(temp_warn, 'cold', 0, 0)
+            self.warn_mgr(temp_warn, 'cold', 0, '', 0)
             
         if cht_val >= 80 and cht_val <= 180:
-            self.warn_mgr(temp_warn, 'green', 0, 0)
+            self.warn_mgr(temp_warn, 'green', 0, '', 0)
 
         if cht_val >= 180.001 and cht_val <= 190:
-            self.warn_mgr(temp_warn, 'yellow', 0, 0)
+            self.warn_mgr(temp_warn, 'yellow', 0, '', 0)
 
         if cht_val > 190:
-            self.warn_mgr(temp_warn, 'red', 0, 0)
+            self.warn_mgr(temp_warn, 'red', 0, '', 0)
 
     # This sets values and defines ranges for the secondary indicators
     def secondary_update(self, dt):
@@ -119,11 +119,11 @@ class GaugeApp(App):
         # Fuel level warning
         level_warn = ids.fuel_warn
         if current_level <= 10:            
-            self.warn_mgr(level_warn, 'red', 1, 0)
+            self.warn_mgr(level_warn, 'red', 1, 'red', 0)
         elif current_level >=10 and current_level <= 25:            
-            self.warn_mgr(level_warn, 'yellow', 1, 0)
+            self.warn_mgr(level_warn, 'yellow', 1, 'yellow', 0)
         elif current_level > 25:            
-            self.warn_mgr(level_warn, 'green', 1, 0)
+            self.warn_mgr(level_warn, 'green', 1, 'green', 0)
         
         # Fuel Flow<-- if available
         try:
@@ -133,19 +133,33 @@ class GaugeApp(App):
 
         # Battery1
         try:
-            batt1 = self.connection.messages['BATTERY_STATUS'].voltages[0]/1000 
-            ids.batt1.value = batt1         
+            batt1 = (self.connection.messages['BATTERY_STATUS'].voltages[0]/1000) * 3
+              
+            if batt1 < ids.batt1.min:
+                ids.batt1.value = ids.batt1.min
+                ids.batt1.label = 'FAULT' 
+                ids.batt1.cadran_color = self.hex_colors['red']                                               
+            else:
+                ids.batt1.value = batt1
+                ids.batt1.label = 'Battery 1\n{:.2f}V'.format(batt1)       
         except:
             batt1 = 0
-            ids.batt1.value = batt1
+            ids.batt1.value = ids.batt1.min
         
         # Battery2 - TEST VALUES
         try:
-            batt2 =self.connection.messages['BATTERY_STATUS'].voltages[0]/1000 #TEST VALUE!!!
-            ids.batt2.value = batt2 
+            batt2 = self.connection.messages['BATTERY_STATUS'].voltages[0]/1000 #TEST VALUE!!!
+             
+            if batt2 < ids.batt2.min:
+                ids.batt2.value = ids.batt2.min
+                ids.batt2.label = 'FAULT'
+                ids.batt2.cadran_color = self.hex_colors['red']                
+            else:
+                ids.batt2.value = batt2
+                ids.batt2.label = 'Battery 2\n{:.2f}V'.format(batt2)
         except:
             batt2 = 0
-            ids.batt2.value = 0
+            ids.batt2.value = ids.batt2.min
 
         self.batt_man(batt1, batt2)
         
@@ -170,22 +184,29 @@ class GaugeApp(App):
     def batt_man(self, batt1_volts, batt2_volts):
         ids = self.root.ids
         batt_warn = ids.batt_warn
-        if batt1_volts < 12 or batt2_volts < 12:            
-            if batt1_volts < 12 and batt2_volts > 12:               
-                self.warn_mgr(batt_warn, 'yellow', 1, 1)
-                ids.batt2.shadow_color = self.hex_colors['green']
-                ids.batt_warn.text = 'Batt 1!'
-            elif batt2_volts < 12 and batt1_volts > 12:                
-                self.warn_mgr(batt_warn, 'yellow', 1, 2)
-                ids.batt1.shadow_color = self.hex_colors['green']
-                ids.batt_warn.text = 'Batt 2!'
-            else:
-                self.warn_mgr(batt_warn, 'yellow', 1, 1)
-                ids.batt2.shadow_color = self.hex_colors['yellow']
-                ids.batt_warn.text = 'Volts!'
-        else:
-            self.warn_mgr(batt_warn, 'green', 1, 1)
-            ids.batt2.shadow_color = self.hex_colors['green']
+        warn = 3.7 # Volts per cell
+        crit = 3.5 # Volts per cell
+        warn_color = ''
+
+        if batt1_volts > warn*12:
+            self.warn_mgr(batt_warn, 'green', 1, 'green', 1)
+
+        if batt2_volts > warn*3:
+            self.warn_mgr(batt_warn, 'green', 1, 'green', 2)            
+
+        if batt1_volts <= warn*12 or batt2_volts <= warn*3:
+            warn_color = 'yellow'
+            if batt1_volts <= crit*12 or batt2_volts <= crit*3:
+                warn_color = 'red'
+                if batt1_volts <=crit*12:
+                    self.warn_mgr(batt_warn, warn_color, 1, 'red', 1)
+                if batt2_volts <=crit*3:
+                    self.warn_mgr(batt_warn, warn_color, 1, 'red', 2)
+
+            if batt1_volts <= warn*12 and batt1_volts > crit*12:
+                self.warn_mgr(batt_warn, warn_color, 1, 'yellow', 1)
+            if batt2_volts <= warn*3 and batt2_volts > crit*3:
+                self.warn_mgr(batt_warn, warn_color, 1, 'yellow', 2)
 
     # Kickoff
     def scheduler(self, dt):
